9293fa4aa2e500f9574952ff2845cca1d21e43e4
Stuti Gupta <juhiexcusestuti@gmail.com>
2025-03-12 20:35:41 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+var options = {
+    strings: ["Young inspiring-influencer @ LI","Web-Developer","Professional-Canva-Designer","Co-Founder @ Alum Connect","Content-creator @ LinkedIn"],
+    typeSpeed: 50, // Speed of typing in milliseconds
+    backSpeed: 50, // Speed of backspacing in milliseconds
+    backDelay: 1000, // Delay before starting to backspace
+    startDelay: 500, // Delay before starting typing
+    loop: true, // Loop the strings infinitely
+    showCursor: true // Show the blinking cursor
+};
+var typed = new Typed("#typed-output", options);
